40becb431a1a4e7db7d5e4dcbb035cd318a9dd17
Diogo Viegas <dviegas@c1r2s10.42lisboa.com>
2026-03-26 12:44:43 +0000

    flood_fill

--- validate_map.c
+++ validate_map.c
@@ -6,11 +6,12 @@
 /*   By: dviegas <dviegas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 11:00:08 by dviegas           #+#    #+#             */
-/*   Updated: 2026/03/26 11:18:17 by dviegas          ###   ########.fr       */
+/*   Updated: 2026/03/26 12:39:03 by dviegas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include "libft/libft.h"
 
 
 int is_valid_char(char c)
@@ -61,13 +62,88 @@ int check_player(t_map *map)
     }
     return (count == 1);
 }
+void find_player(t_map *map, int *x,int *y)
+{
+    int i;
+    int j;
+    i = 0;
+    while(i < map->height)
+    {
+        j = 0;
+        while(j < map->width)
+        {
+            if(is_player(map->grid[i][j]))
+            {
+                *x = i;
+                *y = j;
+                return; 
+            }
+            j++;
+        }
+        i++;
+    }
+}
+char **copy_map(t_map *map)
+{
+    char **copy;
+    int i;
+
+    copy = malloc(sizeof(char *) * map->height);
+    i = 0;
+    while(i < map->height)
+    {
+        copy[i] = ft_strdup(map->grid[i]);
+        i++;
+    }
+    return (copy);
+}
+int flood_fill(char **map,int x, int y,t_map *data)
+{
+    if(x < 0 || y < 0 || x >= data->height || y >= data->width)
+        return (0);
+    if(map[x][y] == ' ')
+        return (0);
+    if(map[x][y] == '1' || map[x][y] == 'V')
+        return (1);
+    map[x][y] = 'V';
+    if(!flood_fill(map,x + 1,y,data))
+        return (0);
+    if(!flood_fill(map,x - 1,y,data))
+        return (0);
+    if(!flood_fill(map,x,y + 1,data))
+        return (0);
+    if(!flood_fill(map,x,y - 1,data))
+        return (0);
+    return (1);
+
+}
+
+int check_closed(t_map *map)
+{
+    int x;
+    int y;
+    char **copy;
+    int valid;
+    find_player(map,&x,&y);
+    copy = copy_map(map);
+    printf("[Player] -> [%d] [%d]\n",x,y);
+    int i = 0;
+    while(i < map->height)
+    {
+        printf("line -> %s\n",copy[i]);
+        i++;
+    }
+    valid = flood_fill(copy,x,y,map);
+    //free_map(map);
+    return (valid);
+}
 int validate_map(t_map *map)
 {
     if(!check_chars(map))
         error_exit("Invalid characters in map");
     if(!check_player(map))
         error_exit("Invalid number of players, only one player is valid");
-    //if(!check_closed(map))
-        //error_exit("Map is not closed\n");
+    if(!check_closed(map))
+        error_exit("Map is not closed\n");
     return (1);
 }
