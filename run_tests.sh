cc0cb3785d4310b8641f2c2c9969e2ef87c7592d
Diogo Viegas <dviegas@c1r2s10.42lisboa.com>
2026-03-26 12:52:13 +0000

    update tester

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,28 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 NC="\033[0m"
 
+echo "==== VALID CONFIGS ===="
+for file in maps/valid_config/*.cub; do
+    ./cub3D "$file" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}OK${NC} $file"
+    else
+        echo -e "${RED}FAIL${NC} $file"
+    fi
+done
+
+echo "==== INVALID CONFIGS ===="
+for file in maps/invalid_config/*.cub; do
+    ./cub3D "$file" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "${GREEN}OK${NC} $file"
+    else
+        echo -e "${RED}FAIL${NC} $file"
+    fi
+done
+
 echo "==== VALID MAPS ===="
-for file in maps/valid/*.cub; do
+for file in maps/valid_map/*.cub; do
     ./cub3D "$file" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo -e "${GREEN}OK${NC} $file"
@@ -15,7 +35,7 @@ for file in maps/valid/*.cub; do
 done
 
 echo "==== INVALID MAPS ===="
-for file in maps/invalid/*.cub; do
+for file in maps/invalid_map/*.cub; do
     ./cub3D "$file" > /dev/null 2>&1
     if [ $? -ne 0 ]; then
         echo -e "${GREEN}OK${NC} $file"
